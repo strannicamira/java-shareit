0c343957a670cbe6d03c515240b196a122c6ecf4
strannicamira <strannicamira@yandex.ru>
2023-08-05 07:59:58 +0300

    Review (1) Delete BookingOutGetDto and so on Clean services from Transactional annotation Back  @ToString.Exclude for Item.owner

--- src/main/java/ru/practicum/shareit/booking/BookingService.java
+++ src/main/java/ru/practicum/shareit/booking/BookingService.java
@@ -1,29 +1,22 @@
 package ru.practicum.shareit.booking;
 
-import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.shareit.item.Item;
 
 import java.util.List;
 
-@Transactional(readOnly = true)
 public interface BookingService {
-    @Transactional
     BookingOutDto createBooking(Integer userId, BookingDto bookingDto);
 
-    @Transactional
     BookingOutDto updateBooking(Integer userId, Integer bookingId, Boolean approved);
 
-    @Transactional(readOnly = true)
     BookingOutDto getBooking(Integer userId, Integer bookingId);
 
-    @Transactional(readOnly = true)
     List<BookingOutDto> getUserBookings(Integer userId, String state);
 
     List<BookingOutDto> getUserItemsBookings(Integer userId, String state);
 
     List<BookingOutDto> getItemsBookingsByUser(Integer item, Integer userId, String state);
 
-    @Transactional
     void deleteBooking(Integer userId, Integer bookingId);
 
     LastBooking getUserItemsLastPastBookings(Integer userId, Item item);
--- src/main/java/ru/practicum/shareit/item/ItemService.java
+++ src/main/java/ru/practicum/shareit/item/ItemService.java
@@ -1,35 +1,27 @@
 package ru.practicum.shareit.item;
 
-import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.shareit.comment.Comment;
 import ru.practicum.shareit.comment.CommentItemDto;
 
 import java.util.List;
 
-@Transactional(readOnly = true)
 public interface ItemService {
 
-    @Transactional(readOnly = true)
     ItemDto getItem(Integer userId, Integer itemId);
 
     List<ItemDto> getUserItems(Integer userId);
 
-    @Transactional(readOnly = true)
     List<ItemDto> getUserItems(Integer userId, String text);
 
-    @Transactional
     ItemDto addNewItem(Integer userId, Item item);
 
     ItemDto updateItem(Integer userId, ItemDto itemDto, Integer itemId);
 
-    @Transactional
     void deleteItem(Integer userId, Integer itemId);
 
-    @Transactional(readOnly = true)
     ItemWithBookingDto getItemWithBooking(Integer userId, Integer itemId);
 
     List<ItemWithBookingDto> getUserItemsWithBooking(Integer userId);
 
-    @Transactional
     CommentItemDto addNewItemComment(Integer userId, Integer itemId, Comment comment);
 }
