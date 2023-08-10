b1e4e9e0d6e4ad6dcaaca035ce012cdfb9845787
strannicamira <strannicamira@yandex.ru>
2023-08-11 00:41:31 +0300

    Add updateUserEmail getUser getUser_whenIdDoesntExist_thenThrowNotFoundException getAllUsers deleteUser

--- src/test/java/ru/practicum/shareit/user/UserServiceImplIntegrationTest.java
+++ src/test/java/ru/practicum/shareit/user/UserServiceImplIntegrationTest.java
@@ -12,6 +12,10 @@ import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.shareit.exception.NotFoundException;
 
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
@@ -89,10 +93,27 @@ public class UserServiceImplIntegrationTest {
         assertThat(savedUser.getEmail(), equalTo(userDto.getEmail()));
     }
 
-    //TODO: updateUserEmail
-
-
     @Order(4)
+    @Test
+    void updateUserEmail() {
+        log.info("UserServiceImplIntegrationTest-" + index++ + "-updateUserName");
+
+        UserDto userDto = makeUserDto("John Doe", "some@email.com");
+        UserDto savedUserDto = userService.createUser(userDto);
+        Integer userId = savedUserDto.getId();
+
+        UserDto userDtoToUpdate = makeUserDtoByEmail("update@email.com");
+
+        userService.updateUser(userId, userDtoToUpdate);
+
+        User savedUser = repository.findById(userId).orElseThrow(() -> new NotFoundException("User not found"));
+
+        assertThat(savedUser.getId(), equalTo(userId));
+        assertThat(savedUser.getName(), equalTo(userDto.getName()));
+        assertThat(savedUser.getEmail(), equalTo(userDtoToUpdate.getEmail()));
+    }
+
+    @Order(5)
     @Test
     void updateUser_whenIdDoesntExist_thenThrowNotFoundException() {
         log.info("UserServiceImplIntegrationTest-" + index++ + "-updateUser_");
@@ -109,6 +130,80 @@ public class UserServiceImplIntegrationTest {
         assertThrows(NotFoundException.class, () -> userService.updateUser(userId, userDtoToUpdate));
     }
 
+    @Order(6)
+    @Test
+    void getUser() {
+        log.info("UserServiceImplIntegrationTest-" + index++ + "-getUser");
+
+        UserDto userDto = makeUserDto("John Doe", "some@email.com");
+
+        UserDto createdUserDto = userService.createUser(userDto);
+        Integer createdUserId = createdUserDto.getId();
+
+        UserDto gotdUserDto = userService.getUser(createdUserId);
+
+        assertThat(createdUserId, equalTo(gotdUserDto.getId()));
+        assertThat(userDto.getName(), equalTo(gotdUserDto.getName()));
+        assertThat(userDto.getEmail(), equalTo(gotdUserDto.getEmail()));
+
+    }
+
+    @Order(7)
+    @Test
+    void getUser_whenIdDoesntExist_thenThrowNotFoundException() {
+        log.info("UserServiceImplIntegrationTest-" + index++ + "-getUser_");
+
+        int userId = 999;
+
+        assertThrows(NotFoundException.class, () -> userService.getUser(userId));
+
+
+    }
+
+
+    @Order(8)
+    @Test
+    void getAllUsers() {
+        log.info("UserServiceImplIntegrationTest-" + index++ + "-getUser");
+        UserDto userDto = makeUserDto("John Doe", "some@email.com");
+        UserDto createdUserDto = userService.createUser(userDto);
+        Integer createdUserId = createdUserDto.getId();
+        List<UserDto> userDtos = Arrays.asList(createdUserDto);
+
+        List<UserDto> gotdUserDtos = userService.getAllUsers();
+
+        assertThat(gotdUserDtos.size(), equalTo(userDtos.size()));
+        for (int i = 0; i < gotdUserDtos.size(); i++) {
+            assertThat(gotdUserDtos.get(i).getId(), equalTo(userDtos.get(i).getId()));
+            assertThat(gotdUserDtos.get(i).getName(), equalTo(userDtos.get(i).getName()));
+            assertThat(gotdUserDtos.get(i).getEmail(), equalTo(userDtos.get(i).getEmail()));
+        }
+
+
+    }
+
+
+    @Order(9)
+    @Test
+    void deleteUser() {
+        log.info("UserServiceImplIntegrationTest-" + index++ + "-deleteUser");
+
+        UserDto userDto = makeUserDto("John Doe", "some@email.com");
+
+        UserDto createdUserDto = userService.createUser(userDto);
+        Integer createdUserId = createdUserDto.getId();
+
+        User user = repository.findById(createdUserId).orElseThrow(() -> new NotFoundException("User not found from saveUser"));
+
+        assertThat(user.getId(), notNullValue());
+        assertThat(user.getName(), equalTo(userDto.getName()));
+        assertThat(user.getEmail(), equalTo(userDto.getEmail()));
+
+        userService.deleteUser(createdUserId);
+        assertThat(repository.findById(createdUserId),equalTo(Optional.empty()));
+    }
+
+
     private UserDto makeUserDto(String name, String email) {
         UserDto dto = new UserDto();
         dto.setName(name);
@@ -119,6 +214,12 @@ public class UserServiceImplIntegrationTest {
     private UserDto makeUserDtoByName(String name) {
         UserDto dto = new UserDto();
         dto.setName(name);
+        return dto;
+    }
+
+    private UserDto makeUserDtoByEmail(String email) {
+        UserDto dto = new UserDto();
+        dto.setEmail(email);
         return dto;
     }
 
