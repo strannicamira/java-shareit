51371f733cf53ab1176db7f1900109d0df03be81
strannicamira <strannicamira@yandex.ru>
2023-08-13 15:37:25 +0300

    Fix run on server port by adding profile default spring.config.activate.on-profile=ci,test,default or just comment this line at all

--- server/src/main/java/ru/practicum/shareit/ShareItServer.java
+++ server/src/main/java/ru/practicum/shareit/ShareItServer.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ShareItApp {
-//TODO: Rename to ShareItServer
-	public static void main(String[] args) {
-		SpringApplication.run(ShareItApp.class, args);
-	}
+public class ShareItServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShareItServer.class, args);
+    }
 
 }
