aa651cdd6e525e6b760268079e228174a8db7c92
strannicamira <strannicamira@yandex.ru>
2023-08-13 10:40:05 +0300

    Add TODO in server pom and app class

--- server/src/main/java/ru/practicum/shareit/ShareItApp.java
+++ server/src/main/java/ru/practicum/shareit/ShareItApp.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ShareItApp {
-
+//TODO: Rename to ShareItServer
 	public static void main(String[] args) {
 		SpringApplication.run(ShareItApp.class, args);
 	}
